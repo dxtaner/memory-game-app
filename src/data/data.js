b380fd634bd8b9912fc2523b1a8eee277eba0839
taner <tanerozer16@gmail.com>
2024-03-25 16:02:20 +0300

    data added

---
+++ src/data/data.js
@@ -0,0 +1,124 @@
+const data = [
+  {
+    id: 1,
+    key: 1,
+    img: "https://img.a.transfermarkt.technology/portrait/header/217111-1701255091.png?lm=1",
+    status: false,
+  },
+  {
+    id: 2,
+    key: 2,
+    img: "https://img.a.transfermarkt.technology/portrait/header/811779-1694614870.jpg?lm=1",
+    status: false,
+  },
+  {
+    id: 3,
+    key: 3,
+    img: "https://img.a.transfermarkt.technology/portrait/header/382528-1671102813.png?lm=1",
+    status: false,
+  },
+  {
+    id: 4,
+    key: 4,
+    img: "https://img.a.transfermarkt.technology/portrait/header/861410-1699472585.jpg?lm=1",
+    status: false,
+  },
+  {
+    id: 5,
+    key: 5,
+    img: "https://img.a.transfermarkt.technology/portrait/header/68863-1671105169.png?lm=1",
+    status: false,
+  },
+  {
+    id: 6,
+    key: 6,
+    img: "https://img.a.transfermarkt.technology/portrait/header/371256-1672830660.jpg?lm=1",
+    status: false,
+  },
+  {
+    id: 7,
+    key: 7,
+    img: "https://img.a.transfermarkt.technology/portrait/header/191614-1698609730.png?lm=1",
+    status: false,
+  },
+  {
+    id: 8,
+    key: 8,
+    img: "https://img.a.transfermarkt.technology/portrait/header/371998-1664869583.jpg?lm=1",
+    status: false,
+  },
+  {
+    id: 9,
+    key: 9,
+    img: "https://img.a.transfermarkt.technology/portrait/header/580195-1667830802.jpg?lm=1",
+    status: false,
+  },
+  {
+    id: 10,
+    key: 10,
+    img: "https://img.a.transfermarkt.technology/portrait/header/412363-1693988167.jpg?lm=1",
+    status: false,
+  },
+  {
+    id: 11,
+    key: 1,
+    img: "https://img.a.transfermarkt.technology/portrait/header/217111-1701255091.png?lm=1",
+    status: false,
+  },
+  {
+    id: 12,
+    key: 2,
+    img: "https://img.a.transfermarkt.technology/portrait/header/811779-1694614870.jpg?lm=1",
+    status: false,
+  },
+  {
+    id: 13,
+    key: 3,
+    img: "https://img.a.transfermarkt.technology/portrait/header/382528-1671102813.png?lm=1",
+    status: false,
+  },
+  {
+    id: 14,
+    key: 4,
+    img: "https://img.a.transfermarkt.technology/portrait/header/861410-1699472585.jpg?lm=1",
+    status: false,
+  },
+  {
+    id: 15,
+    key: 5,
+    img: "https://img.a.transfermarkt.technology/portrait/header/68863-1671105169.png?lm=1",
+    status: false,
+  },
+  {
+    id: 16,
+    key: 6,
+    img: "https://img.a.transfermarkt.technology/portrait/header/371256-1672830660.jpg?lm=1",
+    status: false,
+  },
+  {
+    id: 17,
+    key: 7,
+    img: "https://img.a.transfermarkt.technology/portrait/header/191614-1698609730.png?lm=1",
+    status: false,
+  },
+  {
+    id: 18,
+    key: 8,
+    img: "https://img.a.transfermarkt.technology/portrait/header/371998-1664869583.jpg?lm=1",
+    status: false,
+  },
+  {
+    id: 19,
+    key: 9,
+    img: "https://img.a.transfermarkt.technology/portrait/header/580195-1667830802.jpg?lm=1",
+    status: false,
+  },
+  {
+    id: 20,
+    key: 10,
+    img: "https://img.a.transfermarkt.technology/portrait/header/412363-1693988167.jpg?lm=1",
+    status: false,
+  },
+];
+
+export default data;
